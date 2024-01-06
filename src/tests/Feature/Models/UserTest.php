9d7e67aaf2b325d121af0d95ca286b48bff18e34
yamao ryota <yamao.ryouta@gmail.com>
2024-01-07 08:37:43 +0900

    varchar(255)の動作確認のためにUserTestにテストケースを追加

--- src/tests/Feature/Models/UserTest.php
+++ src/tests/Feature/Models/UserTest.php
@@ -14,6 +14,7 @@ class UserTest extends TestCase
 
     /**
      * @testdox 作成可能なケース
+     * @group model
      * @testWith ["foo", "test@example.com", "qwerty"]
      */
     public function tes_can_create(string $name, string $email, string $password): void
@@ -24,14 +25,37 @@ class UserTest extends TestCase
 
     /**
      * @testdox 必要なカラムが不足しているケース
+     * @group model
      * @testWith [null, "test@example.com", "qwerty"]
      *           ["foo", null, "qwerty"]
      *           ["foo", "test@example.com", null]
      */
     public function test_cannot_create(?string $name, ?string $email, ?string $password): void
     {
-        $this->assertThrows(function () use ($name, $email, $password) {
-            User::create(compact('name', 'email', 'password'));
+        $params = compact('name', 'email', 'password');
+        $this->assertThrows(function () use ($params) {
+            User::create($params);
         });
+    }
+
+    /**
+     * @testdox 名前が長すぎるケース
+     * @group model
+     * @dataProvider tooLongNameProvider
+     */
+    public function test_cannot_create_when_name_is_too_long(string $name, string $email, string $password): void
+    {
+        $params = compact('name', 'email', 'password');
+        $this->assertThrows(function () use ($params) {
+            User::create($params);
+        });
+    }
+
+    public static function tooLongNameProvider(): array
+    {
+        return [
+            [str_repeat("a", 256), "b", "c"],
+            [str_repeat("\u{1f37a}", 256), "b", "c"],
+        ];
     }
 }
